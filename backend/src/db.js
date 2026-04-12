560aff4d0d6e8e868fb1bf05f58564c6d2a4a8fc
Nandini Mundhra <nandinimundhra7@gmail.com>
2026-04-12 19:22:04 +0530

    remove root node_modules and fix env loading

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,16 @@
 import pkg from "pg";
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
+
 const { Pool } = pkg;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+  path: path.resolve(__dirname, "../../.env"),
+});
 
 //PostgreSQL connection setup in Node.js
 const pool = new Pool({
